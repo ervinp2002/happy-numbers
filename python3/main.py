2226e60e079915c38cbd9b8382e91168079cb011
Ervin E. Pangilinan <pangilinan_ee@cobra.cs.mercer.edu>
2022-08-30 15:49:40 -0400

    Implementation in Progress

--- python3/main.py
+++ python3/main.py
@@ -1,9 +1,10 @@
-# Ervin Pangilinan
-# CSC 330: Organization of Programming Languages
-# Project 1: Base 10 Happy Numbers Norms
-# Driver Program in Python3
+"""""
+Ervin Pangilinan
+CSC 330: Organization of Programming Languages
+Project 1: Base 10 Happy Numbers Norms
+Driver Program in Python3
+"""
 
-import itertools
 import math
 
 def getArguments(argsList):
@@ -31,16 +32,56 @@ def getArguments(argsList):
 
 # Function pulled from Rosetta Code. 
 def happy(n):
-    past = set()			
+    # PRE: Integer is passed in as a argument.  
+    # POST: Returns a boolean that determines if the integer is happy or not. 
+
+    # Set allows to check for cycle detection and remove duplicate values.	
+    past = set()        	
     while n != 1:
         n = sum(int(i) ** 2 for i in str(n))
         if n in past:
             return False
-        past.add(n)
+        past.add(n)     # Unique values are added to the set. 
     return True
 
-args = [0, 0]
-getArguments(args)
-print(str(args[0]) + " " + str(args[1]))
+def getHappyNumbers(dictionary, lower, upper):
+    # PRE: Empty dictionary, lower, and upper bounds are passed as arguments. 
+    # POST: Puts norms-happy number pairs in dictionary. 
 
+    for n in range(lower, upper):
+        if (happy(n)): 
+            value = n       # Save the value of the actual happy number. 
+            sequence = set()    
+            sequence.add(n)
 
+            while n != 1:       # While-loop pulled from Rosetta Code. 
+                n = sum(int(i) ** 2 for i in str(n))
+                sequence.add(n)
+
+            norm = math.sqrt(sum(int(i) ** 2 for i in sequence))
+            dictionary.update({norm : value})
+
+def happyPrint(normsList, dictionary):
+    # PRE: Sorted list of norms and dictionary of norm-happy number pairs are filled. 
+    # POST: Prints the happy numbers with the highest norms in descending order. 
+
+    size = 10 if len(normsList) >= 10 else len(normsList)
+    if size == 0: 
+        print("NOBODY'S HAPPY :(")
+    else:
+        for x in range(size): print(dictionary.get(normsList[x])) 
+
+def main():
+    # PRE: Keyboard input is taken to determine range to find happy numbers. 
+    # POST: Outputs in descending order the happy numbers with the highest norms. 
+
+    args = [0, 0]
+    happy = {}
+
+    getArguments(args)
+    getHappyNumbers(happy, args[0], args[1])
+    norms = list(map(float, happy.keys()))
+    norms.sort(reverse = True)
+    happyPrint(norms, happy)
+
+main()
