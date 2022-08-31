9158b095e7baaad2acb3e81d6cba590b9b797faf
Ervin E. Pangilinan <pangilinan_ee@cobra.cs.mercer.edu>
2022-08-31 11:20:41 -0400

    C Implementation in Progress

--- c/main.c
+++ c/main.c
@@ -7,20 +7,46 @@ Driver Program in C
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // Following 3 lines were pulled from Rosetta Code. 
 #define CACHE 256
 enum { h_unknown = 0, h_yes, h_no };
 unsigned char buf[CACHE] = {0, h_yes, 0};
 
-void swap(long *arg1, long *arg2);
-void getArguments(long *lower, long *upper);
-int happy(int n);
-
-struct HappyNumber {
+typedef struct {
     long value;
     double norm;
-};
+} HappyNumber;
+
+int happy(int n);
+void swap(long *arg1, long *arg2);
+void arraySwap(HappyNumber *value1, HappyNumber *value2);
+void getArguments(long *lower, long *upper);
+void quicksort(HappyNumber arr[], int first, int last);
+void pincer(HappyNumber arr[], int first, int last, int *split);
+
+void pincer(HappyNumber arr[], int first, int last, int *split) {
+    // PRE: Called during a quicksort. 
+    // POST: Moves all values less than pivot to the right and greater to the left. 
+
+    int pivot = first;
+    int saveFirst = first;		
+    first++;
+
+    while (first <= last) {
+        while ((arr[first].norm <= arr[pivot].norm) && (first <= last)) last--;
+        while ((arr[last].norm >= arr[pivot].norm) && (first <= last)) first++;  
+        if (first < last) {
+            arraySwap(&arr[first], &arr[last]);
+            first++;	
+            last--;
+        }
+    }
+
+    split = last;
+    arraySwap(&arr[saveFirst], &arr[*split]); 
+}
 
 void swap(long *arg1, long *arg2) {
     // PRE: Two pointers are passed as arguments. 
@@ -29,6 +55,28 @@ void swap(long *arg1, long *arg2) {
     long temp = *arg1;
     *arg1 = *arg2;
     *arg2 = temp;
+}
+
+// Overloaded Swap Function. 
+void arraySwap(HappyNumber *value1, HappyNumber *value2) {
+    // PRE: Happy Numbers that are in the array are passed as arguments. 
+    // POST: Swaps array elements. 
+
+    HappyNumber temp = *value1;
+    *value1 = *value2;
+    *value2 = temp;
+}
+
+void quicksort(HappyNumber arr[], int first, int last) {
+    // PRE: Array of happy numbers, with first and last index are passed in.
+    // POST: Recursively sorts the array by descending norms. 
+
+    if (first < last) {
+        int splitPoint;
+        pincer(arr, first, last, splitPoint);
+        quicksort(arr, first, splitPoint - 1);
+        quicksort(arr, splitPoint + 1, last);
+    }
 }
 
 void getArguments(long *lower, long *upper) {
@@ -80,7 +128,8 @@ int main() {
 
     long lower = 0;
     long upper = 0;
-    
+    HappyNumber list[10];
+
     getArguments(&lower, &upper);
     if (lower > upper) swap(&lower, &upper);
 
