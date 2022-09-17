6b3bc8ca83dc11e8dc46fa9f845a173c8524b731
Ervin E. Pangilinan <pangilinan_ee@cobra.cs.mercer.edu>
2022-09-17 03:32:35 -0400

    Starting Fortran

---
+++ fortran/main.f90
@@ -0,0 +1,17 @@
+! Ervin Pangilinan
+! CSC 330: Organization of Programming Languages
+! Project 1: Base 10 Happy Numbers Norms
+! Main Program in Fortran 90
+
+program Main
+
+    implicit none
+
+    real :: a, b, result
+    a = 15.5
+    b = 25.67
+    result = a + b
+
+    print *, "The total is ", result
+
+end program Main
