5809de62748218f431acdf610816399d94bfc057
Ervin Pangilinan <33070329+ervinp2002@users.noreply.github.com>
2022-09-18 21:52:42 -0400

    Can get input

--- fortran/main.f90
+++ fortran/main.f90
@@ -7,14 +7,25 @@ program Main
 
     implicit none
 
-    ! NOTES TO SELF
+    integer :: arg1
+    integer :: arg2
+    character(len = 15) :: str
+    character(len = 15) :: str2
+
     ! Step 1: Get range from input
+    write(*, "(a)", advance = "no") "First Argument: "
+    read(*, *) str
+    read(str, *) arg1
+
+    write(*, "(a)", advance = "no") "Second Argument: "
+    read(*, *) str2
+    read(str2, *) arg2
+
     ! Step 2: Determine if each number in range is happy
+
     ! Step 3: Find norm of happy number
-    ! Step 4: Keep track of happy numbers with the highest norms. 
 
-    
+    ! Step 4: Keep track of happy numbers with the highest norms.
 
-    
 
 end program Main
