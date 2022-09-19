7aaee0682641703dc0209ae9ce8ec4421d29c501
Ervin Pangilinan <33070329+ervinp2002@users.noreply.github.com>
2022-09-18 23:22:00 -0400

    Started module

--- fortran/main.f90
+++ fortran/main.f90
@@ -3,14 +3,43 @@
 ! Project 1: Base 10 Happy Numbers Norms
 ! Main Program in Fortran 90
 
-program Main
+module happy_nums
+    ! Contains functions for happy numbers
+    implicit none
 
+    contains
+        integer function digit_sum(number) result(sum)
+            ! PRE: An integer is passed in. 
+            ! POST: Returns the squared digit sum of the integer. 
+            implicit none
+
+            integer, intent(in) :: number
+            integer :: digit
+            !integer :: sum
+            integer :: counter
+
+            sum = 0
+            counter = number
+            do while (counter /= 0)
+                digit = mod(counter, 10)
+                sum = digit ** 2
+                counter = counter / 10  
+            end do
+
+        end function digit_sum
+
+end module happy_nums
+
+program Main
+    ! POST: Outputs the 10 happy numbers with the highest norms.
+    use happy_nums
     implicit none
 
     integer :: arg1
     integer :: arg2
-    character(len = 15) :: str
-    character(len = 15) :: str2
+    integer :: num
+    character(len = 20) :: str
+    character(len = 20) :: str2
 
     ! Step 1: Get range from input
     write(*, "(a)", advance = "no") "First Argument: "
@@ -26,6 +55,5 @@ program Main
     ! Step 3: Find norm of happy number
 
     ! Step 4: Keep track of happy numbers with the highest norms.
-
 
 end program Main
