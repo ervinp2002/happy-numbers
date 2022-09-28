146d15ce77604beb57cdd44421325a4b96458c5e
Ervin E. Pangilinan <pangilinan_ee@cobra.cs.mercer.edu>
2022-09-27 22:04:32 -0400

    Starting Implementation

--- go/main.go
+++ go/main.go
@@ -1,7 +1,36 @@
 package main
 
 import "fmt"
+import "math"
+
+
+
+
 
 func main() {
-	fmt.Println("Hello World")
+	// POST:
+
+	var happynums map[float32]int
+	var lower int
+	var upper int
+
+	fmt.Printf("First Argument: ")
+	fmt.Scanln(&lower)
+
+	fmt.Printf("Second Argument: ")
+	fmt.Scanln(&upper)
+
+	if lower > upper {
+		temp = lower
+		lower = upper
+		upper = lower
+	}
+
+	// Step 2: Find happy numbers in range
+	// Step 3: For every happy number, find its norm
+	// Step 4: Push out minimum-norm pair if the map length is 10 or higher
+	// Step 5: Sort after iterating through the range
+	// Step 6: Output the 10 happy numbers with highest norms in descending order
+
 }
+
