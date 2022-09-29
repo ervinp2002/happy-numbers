c6929cda6a428c1a02a3a99c134999bd6d26139a
Ervin E. Pangilinan <pangilinan_ee@cobra.cs.mercer.edu>
2022-09-29 13:18:34 -0400

    First Draft of Implementation

--- go/main.go
+++ go/main.go
@@ -1,16 +1,109 @@
+/*
+Ervin Pangilinan
+CSC 330: Organization of Programming Languages
+Project 1: Base 10 Happy Numbers Norms
+Main Program in Go
+*/
+
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
+func swap(arg1 *int, arg2 *int) {
+	// PRE: Addresses passed in have initialized values.
+	// POST: Swaps the addresses of the passed arguments. 
 
+	temp := *arg1
+	*arg1 = *arg2
+	*arg2 = temp
+}
 
+func digitSum(number int) int {
+	// PRE: An integer is passed in as a argument. 
+	// POST: Returns the squared digit sum of the argument. 
 
+	var sum, digit int = 0, 0
+	for sum = 0; number != 0; number /= 10 {
+		digit = number % 10
+		sum += digit * digit
+	}
+
+	return sum
+}
+
+func isHappy(number int) bool {
+	// PRE: Argument passed in is within the bounds of the input range. 
+	// POST: Boolean value determines if the number is happy or not. 
+
+	/* Determining if a number is happy uses the same methodology as
+	   the C and Fortran implementations. */ 
+	for number != 1 && number != 4 {
+		number = digitSum(number)
+	}
+
+	if number == 4 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func findNorm(number int) float64 {
+	// PRE: Argument passed in has already been verified as a happy number. 
+	// POST: Returns the norm of that happy number. 
+
+	var total float64 = 0
+	if number == 1 {
+		return 1.0
+	} else {
+		total = math.Pow(float64(number), 2) + 1
+		for number != 1 {
+			nextNumber := digitSum(number)
+			total += math.Pow(float64(nextNumber), 2)
+			number = nextNumber
+		}
+
+		return math.Sqrt(total)
+	}	
+}
+
+func findHappy(list map[float64]int, lower int, upper int) {
+	// PRE: Map to hold pairs, upper, and lower bounds have been declared.
+	// POST: Adds happy numbers and their norms to the map. 
+
+	for i := lower; lower <= upper; i++ {
+		if isHappy(i) {
+			norm := findNorm(i)
+			list[norm] = i
+		}
+	}
+}
+
+func printResults(list map[float64]int, normsList []float64) {
+	// PRE: Map is filled and separate list for norms is already sorted. 
+	// POST: Outputs the happy numbers with the highest norms. 
+
+	var limit int
+	if len(list) >= 10 {
+		limit = 10
+	} else {
+		limit = len(list)
+	}
+
+	for i := 0; i < limit; i++ {
+		fmt.Println(list[normsList[i]])
+	}
+}
 
 func main() {
-	// POST:
+	/* POST: Outputs the 10 happy numbers with highest norms 
+		     within the user-inputted range. */ 
 
-	var happynums map[float32]int
+	var happyNums = make(map[float64]int)
 	var lower int
 	var upper int
 
@@ -21,16 +114,17 @@ func main() {
 	fmt.Scanln(&upper)
 
 	if lower > upper {
-		temp = lower
-		lower = upper
-		upper = lower
+		swap(&lower, &upper)
 	}
 
-	// Step 2: Find happy numbers in range
-	// Step 3: For every happy number, find its norm
-	// Step 4: Push out minimum-norm pair if the map length is 10 or higher
-	// Step 5: Sort after iterating through the range
-	// Step 6: Output the 10 happy numbers with highest norms in descending order
+	findHappy(happyNums, lower, upper)
 
+	// Referred to GeekForGeeks for sorting by keys. 
+	norms := make([]float64, len(happyNums))
+ 	for key := range happyNums {
+		norms = append(norms, key)
+	}
+	sort.Sort(sort.Reverse(sort.Float64Slice(norms)))
+
+	printResults(happyNums, norms)
 }
-
