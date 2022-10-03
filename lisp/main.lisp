8109339319efe3dcf973a7212f97582c55956750
Ervin E. Pangilinan <pangilinan_ee@cobra.cs.mercer.edu>
2022-10-03 11:54:32 -0400

    Slowly learning

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -4,20 +4,33 @@
 ; Project 1: Base 10 Happy Number Norms
 ; Main Program in Lisp
 
-(defun digitSum (number)
-   "PRE: An integer is passed in as the argument. 
-    POST: Returns the squared digit sum of the argument."
+(defun square (number)
+    "PRE: Digit is passed in. 
+     POST: Returns the square of the digit."
 
-    (do ((n number (/ n 10))
-         (digit 0 (mod n 10))
-         (sum 0 (+ sum (* digit digit)))
-        )
-        ((= n 0))
-    ) 
+    (* number number)
 )
 
-; isHappy function
+(defun digitSum (number)
+    "PRE: A postive integer is passed in as the argument. 
+     POST: Returns the squared digit sum of the argument."
 
+    (let ((n number) (digit 0) (sum 0))
+        (loop 
+            (setq digit (mod n 10))
+            (setq sum (+ sum (square digit)))
+            (setq n (values (floor n 10)))
+            (when (= n 0) (return sum))
+        )
+    )
+)
+
+(defun isHappy (number)
+    "PRE: A positive integer is passed in as the argument.
+     POST: Returns a boolean to determine if the argument is happy."
+
+    
+)
 
 ; findNorm function
 
@@ -29,24 +42,28 @@
 
 
 ; Main Program, referenced sample code from Lisp email. 
-(progn
-    ; POST: Outputs the 10 happy numbers with the highest norms. 
+; POST: Outputs the 10 happy numbers with the highest norms. 
 
-    (defvar lower)
-    (defvar upper)
+(defvar lower)
+(defvar upper)
 
-    (princ "First Argument: ")
-    (terpri)
-    (setf lower (read))
+(princ "First Argument: ")
+(terpri)
+(setf lower (read))
 
-    (princ "Second Argument: ")
-    (terpri)
-    (setf upper (read))
+(princ "Second Argument: ")
+(terpri)
+(setf upper (read))
 
-    (if (> lower upper)
-        (rotatef lower upper)
-    )
-
-    (princ (digitSum 10))
+(if (> lower upper)
+    (rotatef lower upper)
 )
 
+(princ "Lower is ")
+(princ lower)
+(terpri)
+(princ "Upper is ")
+(princ upper)
+(terpri)
+(princ (digitSum upper))
+(terpri)
